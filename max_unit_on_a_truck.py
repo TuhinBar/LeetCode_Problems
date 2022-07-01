c6e31ac2285d3961c4812a64a3b3441b72ca3e7d
TuhinBar <tuhinbr99@gmail.com>
2022-07-02 00:30:00 +0530

    LeetCode(day4)

---
+++ max_unit_on_a_truck.py
@@ -0,0 +1,18 @@
+# Leetcode: (1710): Maximum unit on a truck (Easy)
+
+def maximumUnits(boxTypes, truckSize):
+    boxTypes.sort(key=lambda x: x[1], reverse = True)
+    ans = 0
+    for i in range (len(boxTypes)):
+        if boxTypes[i][0] <= truckSize:
+            truckSize -= boxTypes[i][0]
+            ans += (boxTypes[i][0]) * boxTypes[i][1]
+        elif boxTypes[i][0] > truckSize:
+            ans += truckSize * boxTypes[i][1]
+            truckSize -= truckSize   
+    return ans
+
+#  Driver code
+boxTypes = [[5,10],[2,5],[4,7],[3,9]]
+truckSize = 10
+print(maximumUnits(boxTypes,truckSize))
