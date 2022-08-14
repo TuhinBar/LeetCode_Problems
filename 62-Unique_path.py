f6c30e1193c1963ea83be128ab0919e0a6faeaca
TuhinBar <tuhinbr99@gmail.com>
2022-08-15 00:10:37 +0530

    Problem 62

--- 62-Unique_path.py
+++ 62-Unique_path.py
@@ -5,6 +5,15 @@ def uniquePaths(m: int, n: int) -> int:
         return 1
     return uniquePaths(m-1,n)+uniquePaths(m,n-1)
 
+# Alternate Solution:
+def uniquePaths(self, m: int, n: int) -> int:
+    dp = [1] * n
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[j] = dp[j - 1] + dp[j]
+    return dp[-1] if m and n else 0
+
+    
 # Test
 print(uniquePaths(3,7))
 
